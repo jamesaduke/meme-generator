6dca20037f3c5e1ccb67a62ae6f479907f3e374d
James Aduke <jamesodwuor4@gmail.com>
2022-03-22 17:53:02 +0300

    meme component markup

---
+++ src/components/Meme.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Meme = () => {
+  return (
+    <div>
+        <form action="" className="form">
+            <input type="text" className='form--input' />
+            <input type="text" className='form--input' />
+
+            <button>Get a new Meme Image 🖼</button>
+        </form>
+    </div>
+  )
+}
+
+export default Meme
