f73e68981fe040a096574b71faca177690af9415
James Aduke <jamesodwuor4@gmail.com>
2022-03-22 17:38:47 +0300

    header css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import  TrollFace from  '../images/trollFace.png';
+import  TrollFace from  '../images/troll_face.png';
 
 
 const Header = () => {
   return (
     <header className='header'>
-        <img src={TrollFace} alt='troll face' />
-        <h1>Meme Generator</h1>
-        <h4>React Course - Project 3</h4>
+        <img src={TrollFace} alt='troll face' className='header--image' />
+        <h1 className='header--title'>Meme Generator</h1>
+        <h4 className='header--project'>React Course - Project 3</h4>
     </header>
   )
 }
